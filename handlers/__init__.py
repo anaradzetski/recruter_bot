0622b38eeabe6d9f174d8ec2ed1532338636c3f3
Andrey Naradzetski <andrey.naradzetski@gmail.com>
2021-05-30 18:20:17 +0300

    avoiding lambdas in loops

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,2 +1,13 @@
 """inits HANDLERS"""
-from .handlers import HANDLERS
+import json
+from telegram.ext import MessageHandler, Filters
+from .handlers import HANDLERS, _SHORTCUTS_FILE, handler
+
+def make_reply_lambda(text):
+    """Tmp function to avoid lambdas in loops."""
+    return lambda update, context: update.message.reply_text(text)
+
+with open(_SHORTCUTS_FILE) as f:
+    shortcuts = json.load(f)
+    for key, val in shortcuts.items():
+        handler(MessageHandler, Filters.regex(key))(make_reply_lambda(val))
