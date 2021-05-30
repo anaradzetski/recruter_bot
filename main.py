622d97a05c2e6e51a9d7ed2a71b8b1da4e6f41ab
Andrey Naradzetski <andrey.naradzetski@gmail.com>
2021-05-30 13:22:35 +0300

    handler decorator and concomitant stuff

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 """Main module"""
 import re
 import logging
-from telegram.ext import Updater, CommandHandler
-from handlers import check
+import inspect
+import telegram.ext as te
 
-TOKEN_FILE = 'token.txt'
+import handlers
+
+def load_token(token_file: 'str' = 'token.txt') -> str:
+    """Loads token from specified file"""
+    with open(token_file) as f:
+        return re.match(r'\S+', f.read()).group(0)
+
+
+def register_handlers(dispatcher: te.Dispatcher) -> None:
+    """Registers handlers from handlers.py"""
+    handler_callbacks = inspect.getmembers(
+            handlers, lambda obj: hasattr(obj, 'handler_inst')
+    )
+    for _, handler_callback in handler_callbacks:
+        dispatcher.add_handler(handler_callback.handler_inst)
+
 
 def main():
     """main function"""
     logging.basicConfig(level=logging.INFO)
-    with open(TOKEN_FILE) as f:
-        token = re.match(r'\S+', f.read()).group(0)
-    updater = Updater(token)
+    updater = te.Updater(load_token())
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('check', check))
+    register_handlers(dispatcher)
     updater.start_polling()
 
 
